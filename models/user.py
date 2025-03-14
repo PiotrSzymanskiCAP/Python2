cc48984fa795e3c4dd3ef5252c22d873ead1a84f
piotr.szymanski <external.piotr.szymanski@kuehne-nagel.com>
2025-03-14 14:54:43 +0100

    Fetching Geo-location

--- models/user.py
+++ models/user.py
@@ -1,23 +1,24 @@
 import uuid
 
+import reverse_geocode
+
+
+def get_country_name(latitude, longitude):
+    location = reverse_geocode.get((latitude, longitude))
+    return location["country"]
+
+
 class User:
 
-   # def __init__(self, first_name, last_name, id, age, lat, lng ):
-    #    self.uuid = uuid.uuid4()
-     #   self.first_name = first_name
-      #  self.last_name = last_name
-       # self.id = id
-        #self.age = age
-        #self.lat = lat
-        #self.lng = lng
-
-
-
-    def __init__(self, id, first_name, last_name):
+    def __init__(self, id, first_name, last_name, age, latitude, longitude):
         self.uuid = uuid.uuid4()
         self._id = id
         self._first_name = first_name
         self._last_name = last_name
+        self._age = age
+        self._lat = latitude
+        self._lng = longitude
+        self._country = get_country_name(self._lat, self._lng)
 
     def __repr__(self):
-        return f"User with ID: {self._id} -> {self._first_name} {self._last_name} \n"
+        return f"User with ID: {self._id} -> {self._first_name} {self._last_name} Age: {self._age} Cord: {self._lat}, {self._lng} -> {self._country}\n"
--- services/user_service.py
+++ services/user_service.py
@@ -2,16 +2,10 @@ import logging
 
 from controllers.user_controller import UserController
 from models.user import User
-import json
+
 
 class UserService:
     user_controller = UserController()
-
-
-  #  def fetch_all_user(self):
-   #     user_data = self.user_controller.get_user_info()
-   #     print(user_data)
-   #     print(len(user_data["users"]))
 
     def fetch_all_users_info(self, page_limit, limit):
         all_users = []
@@ -20,20 +14,19 @@ class UserService:
 
         while total_users < page_limit * limit:
             user_data = self.user_controller.get_user_info(skip, limit)
-            if user_data and user_data['users']:
-                all_users.extend(self.fetch_users_from_data(user_data['users']))
-                logging.info(f"Range: {skip} - {skip+limit}")
+            if user_data and user_data["users"]:
+                all_users.extend(self.fetch_users_from_data(user_data["users"]))
+                logging.info(f"Range: {skip} - {skip + limit}")
                 skip += limit
-                total_users += len(user_data['users'])
+                total_users += len(user_data["users"])
             else:
                 break
         return all_users
 
-
     def fetch_user_info(self, user_id):
         user_data = self.user_controller.get_user_info()
         if user_data:
-            user_dict = {user['id']: user for user in user_data['users']}
+            user_dict = {user["id"]: user for user in user_data["users"]}
             if user_id in user_dict:
                 print(user_dict[user_id]["firstName"])
             else:
@@ -44,6 +37,13 @@ class UserService:
     def fetch_users_from_data(self, user_data):
         all_users = []
         for user in user_data:
-            new_user = User(user["id"], user["firstName"],user["lastName"] )
+            new_user = User(
+                user["id"],
+                user["firstName"],
+                user["lastName"],
+                user["age"],
+                user["address"]["coordinates"]["lat"],
+                user["address"]["coordinates"]["lng"],
+            )
             all_users.append(new_user)
         return all_users
